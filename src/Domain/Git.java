c0108de5b4df64e5a16c54673e7ee05c71461675
Alejandro Hincapie Pulgarin <alejandro.hincapiep@udea.edu.co>
2022-10-31 14:40:20 -0500

    Agregadas variables

--- src/Domain/Git.java
+++ src/Domain/Git.java
@@ -5,6 +5,10 @@ public class Git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Git");
+		
+		String loro = "Lorito"; 
+		
+		
 	}
 
 }
